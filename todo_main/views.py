38a34a96ee094ade4ae296c720c32d2e53fee572
Ogwuegbu Maxwell <53094485+OgwuegbuMaxwell@users.noreply.github.com>
2024-01-20 00:52:07 -0500

    Django Todo App Final Project

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,14 +4,17 @@ from todo.models import Task
 
 
 def home(request):
+    # Uncompleted tasks
     # Filter is used to filter multiple data based on some condition
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     #print(tasks)
     
     # Completed tasks
-    completed_tasks = Task.objects.filter(is_completed=True)
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    print(completed_tasks)
     
     context = {
         'tasks' : tasks,
+        'completed_tasks' : completed_tasks,
     }
     return render(request, 'home.html', context)
